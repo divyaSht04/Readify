5685e69a20867800676acdd30ba529a403963059
Divya Shrestha <divyyyasht5566@gmail.com>
2025-05-04 10:35:59 +0545

    login and registration with JWT

--- Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfe7e010736b5fb5438f97b6b6820931895b3e44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a53840f6700bcc293e18a17e9213b3e34de67ded")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
